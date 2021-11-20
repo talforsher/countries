e44de562e7a8450ba77063b30f5a66420c3b3408
Tal Forsher <talf@webiya.com>
2021-11-20 16:10:28 +0200

    interfacing Countries and Country initial and destructured object of the response from server to be typed for the map result

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,30 @@ import axios from "axios";
 import { Api } from "./constants";
 import "./styles.css";
 
+interface Countries {
+  name: { common: string };
+  flags: { svg: string };
+}
+
+interface Country {
+  name: string;
+  flags: string;
+}
+
 const App = () => {
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState<Country[] | []>([]);
 
   useEffect(() => {
-    axios.get(Api.countries).then((response) => setCountries(response.data));
+    axios.get(Api.countries).then((response) =>
+      setCountries(
+        response.data.map(
+          ({ name: { common: name }, flags: { svg: flag } }: Countries) => ({
+            name,
+            flag
+          })
+        )
+      )
+    );
   }, []);
 
   return <div className="App">{JSON.stringify(countries)}</div>;
